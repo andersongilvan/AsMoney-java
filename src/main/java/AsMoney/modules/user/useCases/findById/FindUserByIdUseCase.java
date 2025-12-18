0409aaf74539351b3edd8f2aea8ba2dcc277aff4
andersongilvan <andersongilvan@gmail.com>
2025-12-18 11:19:48 -0300

    feat: cria a funcionalidade de buscar um usuário pelo id

--- src/main/java/AsMoney/modules/user/useCases/findById/FindUserByIdUseCase.java
+++ src/main/java/AsMoney/modules/user/useCases/findById/FindUserByIdUseCase.java
@@ -1,4 +1,24 @@
 package AsMoney.modules.user.useCases.findById;
 
+
+import AsMoney.modules.user.entity.User;
+import AsMoney.modules.user.exceptions.UserNotFoudException;
+import AsMoney.modules.user.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
 public class FindUserByIdUseCase {
+
+    private final UserRepository userRepository;
+
+    public FindUserByIdUseCase(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User execute(UUID idUser) {
+        return userRepository.findById(idUser)
+                .orElseThrow(UserNotFoudException::new);
+    }
 }
