22ace7eb3522f9945ffd6219888b7f08a4a9719b
andersongilvan <andersongilvan@gmail.com>
2025-12-19 09:56:29 -0300

    feat(transaction): cria o método no repository de transaction que busca todas as transações de um usuário utilizando JPQL (Java Persistence Query Language)

--- src/main/java/AsMoney/modules/transcation/repository/TransactionRepository.java
+++ src/main/java/AsMoney/modules/transcation/repository/TransactionRepository.java
@@ -2,10 +2,24 @@ package AsMoney.modules.transcation.repository;
 
 import AsMoney.modules.transcation.entiry.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface TransactionRepository extends JpaRepository<Transaction, UUID> {
+
+    @Query(
+            """
+            SELECT t 
+            FROM Transaction t
+            JOIN t.user u
+            WHERE u.id = :userId    
+                    """
+    )
+    List<Transaction>findByUser(@Param("userId") UUID userId);
 }
