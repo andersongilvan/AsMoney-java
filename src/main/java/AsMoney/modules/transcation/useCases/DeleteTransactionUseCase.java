9bae5cb7ce1f6a3dceaadbd4a8a4e992718c4ad5
andersongilvan <andersongilvan@gmail.com>
2025-12-19 23:50:04 -0300

    feat(transaction): cria o caso de uso que deleta uma transação

--- src/main/java/AsMoney/modules/transcation/useCases/DeleteTransactionUseCase.java
+++ src/main/java/AsMoney/modules/transcation/useCases/DeleteTransactionUseCase.java
@@ -1,4 +1,23 @@
 package AsMoney.modules.transcation.useCases;
 
+
+import AsMoney.modules.transcation.repository.TransactionRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
 public class DeleteTransactionUseCase {
+
+    private final TransactionRepository transactionRepository;
+
+    public DeleteTransactionUseCase(TransactionRepository transactionRepository) {
+        this.transactionRepository = transactionRepository;
+    }
+
+    public void execute(UUID transactionId, UUID userId) {
+
+        transactionRepository.deleteById(transactionId, userId);
+
+    }
 }
