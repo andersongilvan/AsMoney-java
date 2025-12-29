3e9d4772a717084a9ba1719f76eb999c1ba41562
andersongilvan <andersongilvan@gmail.com>
2025-12-29 15:12:48 -0300

    docs: adiciona a documentação do controller de atualizar uma transação

--- src/main/java/AsMoney/modules/transcation/controllers/Update.java
+++ src/main/java/AsMoney/modules/transcation/controllers/Update.java
@@ -1,14 +1,15 @@
 package AsMoney.modules.transcation.controllers;
 
+
 import AsMoney.config.security.jwt.TokenData;
-import AsMoney.swaggerResponse.RequiredToken;
-import AsMoney.swaggerResponse.Unauthorized;
-import AsMoney.swaggerResponse.ValidationErrors;
 import AsMoney.modules.transcation.dto.TransactionRequestDto;
 import AsMoney.modules.transcation.dto.TransactionsResponse;
 import AsMoney.modules.transcation.entiry.Transaction;
 import AsMoney.modules.transcation.mapper.TransactionMapper;
-import AsMoney.modules.transcation.useCases.RegisterTransactionUseCase;
+import AsMoney.modules.transcation.useCases.UpdateTransactionUseCase;
+import AsMoney.swaggerResponse.RequiredToken;
+import AsMoney.swaggerResponse.Unauthorized;
+import AsMoney.swaggerResponse.ValidationErrors;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
@@ -17,43 +18,43 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
 
-@RequiredArgsConstructor
 @RestController
 @RequestMapping("/asmoney/transaction")
-public class Create {
+@RequiredArgsConstructor
+public class Update {
 
-    private final RegisterTransactionUseCase register;
+    private final UpdateTransactionUseCase update;
 
 
-    @Operation(summary = "Create a Transaction",
+    @Operation(summary = "Update a Transaction",
             description =
                     "## Return Flow\n" +
-                            "Returns the details of a specific transaction.\n" +
+                            "Returns the details of a specific transaction." +
+                            "\n" +
                             "### Success:" +
-                            "\n**201:** Created\n" +
+                            "\n**200:** Ok\n" +
                             "\n" +
                             "### Possible Errors:" +
+                            "\n" +
+                            "**400**: Validation Error.\n" +
                             "\n" +
                             "**401**: Missing or invalid token.\n" +
                             "\n" +
                             "**403**: Forbidden")
 
+
     @SecurityRequirement(name = "bearerAuth")
     @ApiResponses(value = {
 
-            @ApiResponse(responseCode = "201",
-                    description = "Created",
+            @ApiResponse(responseCode = "200",
+                    description = "Ok",
                     content = @Content(mediaType = "application/json",
                             schema = @Schema(implementation = TransactionsResponse.class))),
             @ApiResponse(responseCode = "400", description = "Invalid data",
@@ -69,20 +70,19 @@ public class Create {
                             schema = @Schema(implementation = Unauthorized.class))),
     })
 
-    @PostMapping
+
+    @PutMapping("/{transactionId}")
     public ResponseEntity<TransactionsResponse> handler(@AuthenticationPrincipal TokenData tokenData,
-                                                       @Valid @RequestBody TransactionRequestDto requestDto) {
+                                                        @PathVariable UUID transactionId,
+                                                        @Valid @RequestBody TransactionRequestDto updated) {
 
         UUID userId = UUID.fromString(tokenData.id());
 
-        Transaction transaction = TransactionMapper.toTransaction(requestDto, userId);
+        Transaction transaction = TransactionMapper.toTransaction(updated, userId);
 
-        Transaction result = this.register.execute(transaction);
+        Transaction result = this.update.execute(transactionId, transaction);
 
-        TransactionsResponse transactionsResponse = TransactionMapper.toTransactionResponse(result);
-
-        return ResponseEntity.status(HttpStatus.CREATED).body(transactionsResponse);
-
+        return ResponseEntity.ok(TransactionMapper.toTransactionResponse(result));
 
     }
 
